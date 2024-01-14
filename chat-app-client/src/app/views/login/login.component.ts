521f73fc9967b70bdcf534d4f51e6751e3eda7db
duongtran2003 <dt005250@gmail.com>
2024-01-14 20:33:45 +0700

    added a bit of styling. I hate this.

--- chat-app-client/src/app/views/login/login.component.ts
+++ chat-app-client/src/app/views/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGoogle, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  
+
+  googleIcon = faGoogle;
+  facebookIcon = faFacebook;
+  twitterIcon = faTwitter; 
 }
