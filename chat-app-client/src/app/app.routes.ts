187ccb5f44794135285ffefdbe380ac3e6852d65
duongtran2003 <dt005250@gmail.com>
2024-01-14 11:46:48 +0700

    added tailwind, fontawesome and a simple routing

--- chat-app-client/src/app/app.routes.ts
+++ chat-app-client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './views/register/register.component';
+import { LoginComponent } from './views/login/login.component';
+import { MainComponent } from './views/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: MainComponent },
+];
