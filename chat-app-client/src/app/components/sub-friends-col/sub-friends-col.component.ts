34cafad32fcfc3606107be88f8e770924f262973
duongtran2003 <dt005250@gmail.com>
2024-04-16 18:24:28 +0700

    populate friend list

--- chat-app-client/src/app/components/sub-friends-col/sub-friends-col.component.ts
+++ chat-app-client/src/app/components/sub-friends-col/sub-friends-col.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { FriendsService } from '../../services/friends.service';
 
 @Component({
   selector: 'app-sub-friends-col',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './sub-friends-col.component.html',
   styleUrl: './sub-friends-col.component.css'
 })
-export class SubFriendsColComponent {
+export class SubFriendsColComponent implements OnInit {
+  private userService = inject(UserService);
+  private friendListService = inject(FriendsService);
 
+  currentUser: any;
+  currentFriendList: any;
+
+  constructor() {
+    this.currentFriendList = [];
+    this.currentUser = {};
+  } 
+
+  ngOnInit(): void {
+    this.currentUser = this.userService.getUser();
+    this.currentFriendList = this.currentUser.friends;
+  }
 }
