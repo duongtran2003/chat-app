6ff89b200a04737c0e852e5710cc1077986c632b
duongtran2003 <dt005250@gmail.com>
2024-05-08 05:30:38 +0700

    friend request check exist

--- chat-app-server/src/controllers/friendRequest.ts
+++ chat-app-server/src/controllers/friendRequest.ts
@@ -9,24 +9,52 @@ class FriendRequestController {
 
     const user = await User.findById(userId);
     const recipient = req.body.recipient;
+
+    const request1 = await FriendRequest.findOne({ sender: userId, recipient: recipient });
+    const request2 = await FriendRequest.findOne({ sender: recipient, recipient: userId });
+
+    if (request1 || request2) {
+      return res.status(201).json(request1 ? request1 : request2);
+    }
+
     FriendRequest.create({
       sender: userId,
       recipient: recipient
     })
-    .then((newRequest) => {
-      return res.status(201).json(newRequest);
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        "message": err.message
+      .then((newRequest) => {
+        return res.status(201).json(newRequest);
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          "message": err.message
+        });
       });
-    });
   }
-  
+
+  async index(req: Request, res: Response) {
+    const userId = res.locals.claims.userId;
+    const otherUserId = req.query.id;
+    if (!otherUserId) {
+      const requests = await FriendRequest.find({ recipient: userId });
+    }
+    else {
+      const request1 = await FriendRequest.findOne({ sender: userId, recipient: otherUserId });
+      const request2 = await FriendRequest.findOne({ sender: otherUserId, recipient: userId });
+      if (request1 || request2) {
+        return res.status(200).json(request1 ? request1 : request2);
+      }
+      else {
+        return res.status(404).json({
+          message: "not found",
+        });
+      }
+    }
+  }
+
   async handleRequest(req: Request, res: Response) {
     const { requestId, action } = req.body;
     const friendRequest = await FriendRequest.findById(requestId);
-    const targetId = friendRequest!.recipient; 
+    const targetId = friendRequest!.recipient;
 
     if (action == "accept") {
       const userId = res.locals.claims.userId;
@@ -44,14 +72,8 @@ class FriendRequestController {
       return res.status(200).json({ "message": "success" });
     }
   }
-  
-  async index(req: Request, res: Response) {
-    const userId = res.locals.claims.userId;
-    const requests = await FriendRequest.find({ recipient: userId })
-    return res.status(200).json(requests);
-  }
 }
 
 export {
-  FriendRequestController 
+  FriendRequestController
 }
